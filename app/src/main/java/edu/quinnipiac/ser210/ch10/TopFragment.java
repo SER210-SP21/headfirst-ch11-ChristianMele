40fd437708c2d4e1bbbe223fbd3b675a102d0187
Chris <christianmmele1998@gmail.com>
2020-03-03 08:55:10 -0500

    Initial commit

---
+++ app/src/main/java/edu/quinnipiac/ser210/ch10/TopFragment.java
@@ -0,0 +1,17 @@
+package edu.quinnipiac.ser210.ch10;
+
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class TopFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_top, container, false);
+    }
+}
